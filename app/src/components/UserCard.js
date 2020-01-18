65358580cd062faf3d36d8dd5e7045e9b1ef051b
Raudel Flores <rawdell@outlook.com>
2020-01-18 14:50:54 -0600

    added the rel='' attribute to increase security

--- app/src/components/UserCard.js
+++ app/src/components/UserCard.js
@@ -5,7 +5,7 @@ const UserCard = (props) => {
         <div className='user-card'>
             <div className='cardHeader'>
                 <img src={props.user.avatar_url} alt='Avatar of Github User'/>
-                <h2>{props.user.name}</h2>
+                <h2>{props.user.login}</h2>
             </div>
             <div className='cardInfo'>
                 <p><strong>Repos:</strong> {props.user.public_repos}</p>
@@ -13,7 +13,7 @@ const UserCard = (props) => {
                 <p><strong>Following:</strong> {props.user.following}</p>
             </div>
             <button className='git-btn'>
-                <a href={props.user.html_url} target='_blank'>Github</a>
+                <a href={props.user.html_url} target='_blank' rel='noopener noreferrer'><strong>Github</strong></a>
             </button>
         </div>
     )

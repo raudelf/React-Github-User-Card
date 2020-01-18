f8b02f0ec54eab7c711231cfdca197f1a7bf1b5d
Raudel Flores <rawdell@outlook.com>
2020-01-17 22:50:13 -0600

    created a fetch to retrieve follower data. implemented the UserCard with map to create cardds for all followers. Still need to style and fix the repo, follower, and following count for user data

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import UserCard from './components/UserCard';
 import './App.css';
 
 class App extends Component {
   state = {
-    myGitData: []
+    myGitData: [],
+    followerData: []
   }
 
   componentDidMount() {
     fetch('https://api.github.com/users/raudelf')
       .then(res => res.json())
-      .then(data => {
-        console.log('RF: fetch > data: ', data)
-        this.setState({...this.state, myGitData: data})
+      .then(myData => {
+        console.log('RF: fetch > My Data: ', myData)
+        this.setState({...this.state, myGitData: myData})
       })
       .catch(err => console.log('RF: Error on Data Fetch: ', err));
+
+    fetch('https://api.github.com/users/raudelf/followers')
+      .then(res => res.json())
+      .then(followerData => {
+        console.log('RF: fetch > Follower Data: ', followerData)
+        this.setState({...this.state, followerData: followerData})
+      })
   }
 
   render () {
@@ -22,6 +31,12 @@ class App extends Component {
         <header className='header'>
           <h1>Github User Cards</h1>
         </header>
+        <UserCard user={this.state.myGitData}/>
+        {this.state.followerData.map((gitUser, index) => {
+          return (
+            <UserCard user={gitUser} key={index}/>
+          )
+        })}
       </div>
     );
   }

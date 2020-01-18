cc2ce37c2a22ffb684127a0de5fbcaab7f3a76a5
Raudel Flores <rawdell@outlook.com>
2020-01-17 22:16:04 -0600

    created a UserCard component that receives data from state to display a Github usercard

---
+++ app/src/components/UserCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const UserCard = (props) => {
+    return (
+        <div className='user-card'>
+            <div className='cardHeader'>
+                <img src={props.user.avatar_url} alt='Avatar of Github User'/>
+                <h2>{props.user.name}</h2>
+            </div>
+            <div className='cardInfo'>
+                <p><strong>Repos:</strong> {props.user.public_repos}</p>
+                <p><strong>Followers:</strong> {props.user.followers}</p>
+                <p><strong>Following:</strong> {props.user.following}</p>
+            </div>
+            <button className='git-btn'>
+                <a href={props.user.html_url}>Github</a>
+            </button>
+        </div>
+    )
+}
+
+export default UserCard;

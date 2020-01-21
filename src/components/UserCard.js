603042006cccdeb31b5b92cca77f6953263d3ee1
Raudel Flores <rawdell@outlook.com>
2020-01-21 16:18:25 -0600

    removed some info from the user cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -7,11 +7,6 @@ const UserCard = (props) => {
                 <img src={props.user.avatar_url} alt='Avatar of Github User'/>
                 <h2>{props.user.login}</h2>
             </div>
-            <div className='cardInfo'>
-                <p><strong>Repos:</strong> {props.user.public_repos}</p>
-                <p><strong>Followers:</strong> {props.user.followers}</p>
-                <p><strong>Following:</strong> {props.user.following}</p>
-            </div>
             <button className='git-btn'>
                 <a href={props.user.html_url} target='_blank' rel='noopener noreferrer'><strong>Github</strong></a>
             </button>
